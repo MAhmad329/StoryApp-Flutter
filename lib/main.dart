3c88f5954c23af75aa7d172b4e66c9c4bc756cc6
Muhammad Ahmad <m_ahmad329@yahoo.com>
2022-12-06 02:14:17 +0500

    Story App

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class Destini extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: StoryPage(),
     );
